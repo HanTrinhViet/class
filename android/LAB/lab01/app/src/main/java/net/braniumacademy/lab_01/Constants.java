857eeeb4e06fd87dc73bbc47b563daf64e8d26c6
Han Trinh Viet <hntrnn12@gmail.com>
2024-01-22 05:17:41 +0700

    commit 22/01/2024

---
+++ android/LAB/lab01/app/src/main/java/net/braniumacademy/lab_01/Constants.java
@@ -0,0 +1,5 @@
+package net.braniumacademy.lab_01;
+
+public final class Constants {
+    public static String[] HOMETOWNS =  {"Hà Nội", "Hà Nam", "Nam Định", "Hải Dương", "Thanh Hóa", "Phú Thọ"};
+}
